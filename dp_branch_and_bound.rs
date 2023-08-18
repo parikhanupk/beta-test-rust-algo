58a7e2b9ff49ddf46ebd45ba383ce0189fd00eaa
Anup Parikh <84572003+parikhanupk@users.noreply.github.com>
2023-08-18 18:42:20 +0530

    Create dp_branch_and_bound.rs

---
+++ dp_branch_and_bound.rs
@@ -0,0 +1,298 @@
+struct Prng {
+    seed: u32,
+}
+
+impl Prng {
+    #[allow(dead_code)]
+    fn new() -> Self {
+        let mut prng = Self { seed: 0 };
+        prng.randomize();
+        return prng;
+    }
+
+    fn randomize(&mut self) {
+        let millis = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_millis();
+        self.seed = millis as u32;
+    }
+
+    // Return a pseudorandom value in the range [0, 2147483647].
+    fn next_u32(&mut self) -> u32 {
+        self.seed = self.seed.wrapping_mul(1_103_515_245).wrapping_add(12_345);
+        self.seed %= 1 << 31;
+        return self.seed;
+    }
+
+    // Return a pseudorandom value in the range [0.0, 1.0).
+    fn next_f64(&mut self) -> f64 {
+        let f = self.next_u32() as f64;
+        return f / (2147483647.0 + 1.0);
+    }
+
+    // Return a pseudorandom value in the range [min, max).
+    fn next_i32(&mut self, min: i32, max: i32) -> i32 {
+        let range = (max - min) as f64;
+        let result = min as f64 + range * self.next_f64();
+        return result as i32;
+    }
+
+    // Return a pseudorandom value in the range [min, max).
+    #[allow(dead_code)]
+    fn next_i64(&mut self, min: i64, max: i64) -> i64 {
+        let range = (max - min) as f64;
+        let result = min as f64 + range * self.next_f64();
+        return result as i64;
+    }
+}
+
+
+
+// Make some random items.
+fn make_items(
+    prng: &mut Prng,
+    num_items: i32,
+    min_value: i32,
+    max_value: i32,
+    min_weight: i32,
+    max_weight: i32,
+) -> Vec<Item> {
+    let mut items: Vec<Item> = Vec::with_capacity(num_items as usize);
+    for _ in 0..num_items {
+        let item = Item {
+            value: prng.next_i32(min_value, max_value),
+            weight: prng.next_i32(min_weight, max_weight),
+            is_selected: false,
+        };
+        items.push(item);
+    }
+    return items;
+}
+
+
+
+// Return a copy of the items.
+fn copy_items(items: &mut Vec<Item>) -> Vec<Item> {
+    let mut new_items: Vec<Item> = Vec::with_capacity(items.len());
+    for item in items {
+        let new_item = Item {
+            value: item.value,
+            weight: item.weight,
+            is_selected: item.is_selected,
+        };
+        new_items.push(new_item);
+    }
+    return new_items;
+}
+
+
+
+// Return the total value of the items.
+// If add_all is true, add up all items.
+// If add_all is false, only add up the selected items.
+fn sum_values(items: &Vec<Item>, add_all: bool) -> i32 {
+    if add_all {
+        return items.iter().map(|item| item.value).sum();
+    } else {
+        return items
+            .iter()
+            .filter(|item| item.is_selected)
+            .map(|item| item.value)
+            .sum();
+    }
+}
+
+
+
+// Return the total weight of the items.
+// If add_all is false, only add up the selected items.
+// If add_all is true, add up all items.
+fn sum_weights(items: &Vec<Item>, add_all: bool) -> i32 {
+    if add_all {
+        return items.iter().map(|item| item.weight).sum();
+    } else {
+        return items
+            .iter()
+            .filter(|item| item.is_selected)
+            .map(|item| item.weight)
+            .sum();
+    }
+}
+
+
+
+// Return the value of this solution.
+// If the solution is too heavy, return -1 so we prefer an empty solution.
+#[allow(dead_code)]
+fn solution_value(items: &Vec<Item>, allowed_weight: i32) -> i32 {
+    // If the solution's total weight > allowed_weight,
+    // return -1 so even an empty solution is better.
+    if sum_weights(items, false) > allowed_weight {
+        return -1;
+    }
+
+    // Return the sum of the selected values.
+    return sum_values(items, false);
+}
+
+
+
+fn print_items(items: &Vec<Item>, all: bool) {
+    let mut num_printed = 0;
+    for i in 0..items.len() {
+        if all || items[i].is_selected {
+            print!("{}({}, {}) ", i, items[i].value, items[i].weight)
+        }
+        num_printed += 1;
+        if num_printed > 100 {
+            println!("...");
+            return;
+        }
+    }
+    println!();
+}
+
+
+
+// Run the algorithm. Display the elapsed time and solution.
+fn run_algorithm(
+    alg: &dyn Fn(&mut Vec<Item>, i32) -> (Vec<Item>, i32, i32),
+    items: &mut Vec<Item>,
+    allowed_weight: i32,
+) {
+    // Copy the items so the run isn't influenced by a previous run.
+    let mut test_items = copy_items(items);
+
+    let start = Instant::now();
+
+    // Run the algorithm.
+    let solution: Vec<Item>;
+    let total_value: i32;
+    let function_calls: i32;
+    (solution, total_value, function_calls) = alg(&mut test_items, allowed_weight);
+
+    let duration = start.elapsed();
+    println!("Elapsed: {:?}", duration);
+
+    print_items(&solution, false);
+    println!(
+        "Value: {}, Weight: {}, Calls: {}",
+        total_value,
+        sum_weights(&solution, false),
+        function_calls
+    );
+    println!();
+}
+
+
+
+fn branch_and_bound(items: &mut Vec<Item>, allowed_weight: i32) -> (Vec<Item>, i32, i32) {
+    return do_branch_and_bound(items, allowed_weight, 0, 0, 0, sum_values(items, true), 0);
+}
+
+
+
+fn do_branch_and_bound(
+    items: &mut Vec<Item>,
+    allowed_weight: i32,
+    mut best_value: i32,
+    current_value: i32,
+    current_weight: i32,
+    remaining_value: i32,
+    next_index: i32,
+) -> (Vec<Item>, i32, i32) {
+    if (next_index as usize) >= items.len() {
+        return (copy_items(items), current_value, 1);
+    } else {
+        if current_value + remaining_value <= best_value {
+            return (vec![], current_value, 1);
+        }
+        let (mut included_solution, mut included_value, mut included_calls) = (vec![], 0, 1);
+        if current_weight + items[next_index as usize].weight <= allowed_weight {
+            items[next_index as usize].is_selected = true;
+            (included_solution, included_value, included_calls) = do_branch_and_bound(
+                items,
+                allowed_weight,
+                best_value,
+                current_value + items[next_index as usize].value,
+                current_weight + items[next_index as usize].weight,
+                remaining_value - items[next_index as usize].value,
+                next_index + 1,
+            );
+            if included_value > best_value {
+                best_value = included_value;
+            }
+        }
+        items[next_index as usize].is_selected = false;
+        let (excluded_solution, excluded_value, excluded_calls) = do_branch_and_bound(
+            items,
+            allowed_weight,
+            best_value,
+            current_value,
+            current_weight,
+            remaining_value - items[next_index as usize].value,
+            next_index + 1,
+        );
+        if included_value >= excluded_value {
+            return (
+                included_solution,
+                included_value,
+                included_calls + excluded_calls + 1,
+            );
+        } else {
+            return (
+                excluded_solution,
+                excluded_value,
+                excluded_calls + included_calls + 1,
+            );
+        }
+    }
+}
+
+
+
+use std::time::Instant;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+const NUM_ITEMS: i32 = 40; // A reasonable value for branch and bound search.
+
+const MIN_VALUE: i32 = 1;
+const MAX_VALUE: i32 = 10;
+const MIN_WEIGHT: i32 = 4;
+const MAX_WEIGHT: i32 = 10;
+
+struct Item {
+    value: i32,
+    weight: i32,
+    is_selected: bool,
+}
+
+fn main() {
+    // Prepare a Prng using the same seed each time.
+    let mut prng = Prng { seed: 1337 };
+    prng.randomize();
+
+    // Make some random items.
+    let mut items = make_items(
+        &mut prng, NUM_ITEMS, MIN_VALUE, MAX_VALUE, MIN_WEIGHT, MAX_WEIGHT,
+    );
+    let allowed_weight = sum_weights(&items, true) / 2;
+
+    // Display basic parameters.
+    println!("*** Parameters ***");
+    println!("# items:        {}", NUM_ITEMS);
+    println!("Total value:    {}", sum_values(&items, true));
+    println!("Total weight:   {}", sum_weights(&items, true));
+    println!("Allowed weight: {}", allowed_weight);
+    print_items(&items, true);
+    println!();
+
+    if NUM_ITEMS > 40 {
+        // Only run branch and bound search if num_items is small enough.
+        println!("Too many items for branch and bound search\n");
+    } else {
+        println!("*** Branch and Bound Search ***");
+        run_algorithm(&branch_and_bound, &mut items, allowed_weight);
+    }
+}
